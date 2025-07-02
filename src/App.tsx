04f262053200cc0563ce460686b4ae09098042e6
Alifa Ara Heya <alifaaraheya@gmail.com>
2025-07-02 15:49:06 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Library Managemenet System</h1>;
+}
+
+export default App;
---
+++ src/routes/index.ts
@@ -0,0 +1,19 @@
+
+import { createBrowserRouter } from "react-router";
+import App from "../App";
+import Books from "../pages/Books";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        Component: App,
+        children: [
+            {
+                index: true,
+                Component: Books
+            }
+        ]
+    }
+])
+
+export default router;
